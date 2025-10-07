cea1477ae4b91c50db68fa36da8efb901d3529de
root <Johnaniebiet@gmail.com>
2025-10-07 12:10:51 -0700

    added arithmetic_operations.py to perform operations when called

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,20 +1,14 @@
 def perform_operation(num1, num2, operation):
-   
-    num1 = float(num1)
-    num2 = float(num2)
-    operation = str(operation).lower()
-    
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            if num2 == 0:
-                return("error: division by zero")
-            else:
-                return num1 / num2
-        case _ :
-            print("no such operation")
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+    else:
+        return "Invalid operation"
