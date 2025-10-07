f5c4b6a8cc232573d5e83e4032c2a7695193ec22
root <Johnaniebiet@gmail.com>
2025-10-07 11:59:44 -0700

    added arithmetic_operations.py to perform operations when called

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,14 @@
+    match operation:
+        case 'add':
+            return num1 + num2
+        case 'subtract':
+            return num1 - num2
+        case 'multiply':
+            return num1 * num2
+        case 'divide':
+            if num2 == 0:
+                return("error: division by zero")
+            else:
+                return num1 / num2
+        case _ :
+            print("no such operation")
